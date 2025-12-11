949b5a84258b23047a0b53f38f2fabf66b34a024
jyq <jyq311601@qq.com>
2025-12-11 17:22:03 +0800

    search

--- car.h
+++ car.h
@@ -2,13 +2,16 @@
 void add ();
 void delete(int address);
 double  get_money(int time);
+void search(int addresss);
+void desplay_all();
+
+
 void search(int addresss){
      for(int i = 0;i < car_count;i++){
          if(car[i].address == address){
-	   printf("%s\n%d\n%.lf\n%d", car[i].num, car[i].time, car[i].money, car[i].address);
-	 }
-	 break;
+           printf("车牌号为：%s\n停车时间为：%d\n停车费用为：%.lf\n车位为：%d", car[i].num, car[i].time, get_money(car[i].time), car[i].address);
+         }
+         break;
      }
 }
-void desplay_all();
 
