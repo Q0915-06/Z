513ae724066b6fdb7518c36f7591283ef3380fa9
zhang <3444029759@qq.com>
2025-12-22 21:58:48 +0800

    qingche

--- car.h
+++ car.h
@@ -16,45 +16,45 @@ void search(int address);
 void desplay_all();
 
 
-void search(int address){
-     for(int i = 0;i < car_count;i++){
-         if(vehicle[i].address == address){
-           printf("车牌号为：%s\n停车时间为：%d\n停车费用为：%.lf\n车位为：%d", vehicle[i].num, vehicle[i].time, get_money(vehicle[i].time), vehicle[i].address);
-         }
-         break;
-     }
+void search(int address) {
+    for (int i = 0; i < car_count; i++) {
+        if (vehicle[i].address == address) {
+            printf("车牌号为：%s\n停车时间为：%d\n停车费用为：%.lf\n车位为：%d", vehicle[i].num, vehicle[i].time, get_money(vehicle[i].time), vehicle[i].address);
+        }
+        break;
+    }
 }
 
 void delete_vehicle(int address)
 {
-        int i,j;
-for(i=0;i<car_count;i++)
-{
-  if(vehicle[i].address==address)
+    int i, j;
+    for (i = 0; i < car_count; i++)
+    {
+        if (vehicle[i].address == address)
         {
-        for(j=i;j<car_count-1;j++)
-                {
-                vehicle[j]=vehicle[j+1];
-                }
-                car_count--;
-                printf("第%d辆车已出库",address);
+            for (j = i; j < car_count - 1; j++)
+            {
+                vehicle[j] = vehicle[j + 1];
+            }
+            car_count--;
+            printf("第%d辆车已出库", address);
         }
-}
+    }
 }
 void desplay_all()
 {
-        if(car_count==0)
-        {
-                printf("No vehicles in the parking lot\n");
-                return ;
-        }
-printf("=====Parking Lot Vehicle Information=====\n");
-printf("Parking Space No.\tLicense Plate\tParking Duration\tFee\n");
-for(int i=0;i<car_count;i++)
-{
-        printf("%d\t%s\t%d\t%.2lf\n",vehicle[i].address,vehicle[i].num,vehicle[i].time,get_money(vehicle[i].time));
-                        }
-                        }
+    if (car_count == 0)
+    {
+        printf("No vehicles in the parking lot\n");
+        return;
+    }
+    printf("=====Parking Lot Vehicle Information=====\n");
+    printf("Parking Space No.\tLicense Plate\tParking Duration\tFee\n");
+    for (int i = 0; i < car_count; i++)
+    {
+        printf("%d\t%s\t%d\t%.2lf\n", vehicle[i].address, vehicle[i].num, vehicle[i].time, get_money(vehicle[i].time));
+    }
+}
 
 
 double get_money(int time) {
@@ -72,7 +72,7 @@ void add() {
     scanf("%s", vehicle[car_count].num);
     printf("请输入停车时间（小时）：\n");
     scanf("%d", &vehicle[car_count].time);
-    printf("请输入停车位编号：\n" );
+    printf("请输入停车位编号：\n");
     scanf("%d", &vehicle[car_count].address);
     car_count++;
     printf("车辆入库成功！\n");
@@ -118,3 +118,4 @@ int main() {
 
     return 0;
 }
+
